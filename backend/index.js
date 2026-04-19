66bb9167f743a271f81d4f29c9bca412e1a5c9e2
Meghana-Chakravarthi <24wh1a05x2@bvrithyderabad.edu.in>
2026-04-19 18:48:33 +0530

    Added CORS properly

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
